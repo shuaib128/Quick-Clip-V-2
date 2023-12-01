c639b5d77957c0370b71567f7e27aed6c83da47f
sami <admin@seattledevicerepair.com>
2023-11-30 17:25:38 -0800

    first commit

---
+++ public/electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+function createWindow() {
+  const startUrl = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : process.env.ELECTRON_START_URL;
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  win.loadURL(startUrl);
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+}
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Shuaib Ahamed is <code>Coding!</code>
+          <button>
+            Click me For check
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
